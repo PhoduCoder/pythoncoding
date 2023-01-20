327df1904341bff8e01c1ac0e1468c855998a6aa
Gaurav <gauravprashar17@gmail.com>
2023-01-20 17:33:18 -0500

    Abstract Class and Interfaces in Python

---
+++ OOPS-Abstract-Class.py
@@ -0,0 +1,72 @@
+Python-OOPS Notes
+
+What is an abstract class? 
+What is an interface?
+
+An abstract class is a way in python to describe
+the basic methods that the implementing class 
+should implement.
+
+An abstract class can have both concrete and abstract methods
+Concrete methods are implemented in abstract class
+while abstract methods have no implementation.
+
+Any inherited class that inherits the abstract class
+will have to implement the abstract methods 
+and MAY override the concrete method of the abstract class if needed
+
+====
+
+from abc import ABC, abstractmethod
+class Shape(ABC):
+    def draw(self):
+        print("drawing shape")
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width * self.height
+    def draw(self):
+        print("drawing rectangle")
+
+======
+
+n this example, Shape is an abstract class with a 
+concrete method draw and an abstract method area.
+Rectangle is a class that inherits from the Shape class 
+and overrides the draw method to provide a different 
+implementation.
+
+======
+
+Interfaces in Python are a very similar concept
+but in interfaces you can't have any concrete methods
+You always have to have abstract methods.
+
+from abc import ABC, abstractmethod
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width * self.height
+
+====
+It's worth noting that, when a class implements an interface,
+it must implement all the abstract methods defined in that 
+interface. If a class does not implement all the methods,
+ it will result in a TypeError.
+====
+
+
+
